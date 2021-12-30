8177fb9169c241309dc3e41ed6da2df7e8128002
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2021-12-30 17:45:39 +0530

    Create pattern19.cpp

---
+++ pattern19.cpp
@@ -0,0 +1,33 @@
+/*    1
+    1 2 1
+  1 2 3 2 1
+1 2 3 4 3 2 1
+*/
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int i=1;
+while(i<=n){
+    int space=n-i;
+    while(space){
+        cout<<" ";
+        space--;
+    }
+    int j=1;
+    while(j<=i){
+        cout<<j;
+        j++;
+    }
+    int value=i-1;
+    while(value){
+        cout<<value;
+        value--;
+    }
+    
+    cout<<"\n";
+    
+  i++;
+}
+}
