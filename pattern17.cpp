d46e45cebde743e65e6a8fe574f194cc4584d13d
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2021-12-30 10:24:15 +0530

    Create pattern17.cpp

---
+++ pattern17.cpp
@@ -0,0 +1,24 @@
+/*3
+A 
+B C 
+C D E */
+#include <iostream>
+using namespace std;
+int main(){
+int i=1,n;
+char value='A';
+cin>>n;
+while(i<=n){
+    int j=1;
+    char tp=value;
+  while(j<=i){
+    cout<<tp<<" ";
+    tp++;
+    j++;
+    
+  }
+   value++;
+  cout<<"\n";
+  i++;
+}
+}
