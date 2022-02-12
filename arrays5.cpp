12b5ab706acf872a77fdc8265623405bdc53828a
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2022-02-12 17:50:46 +0530

    Create arrays5.cpp

---
+++ arrays5.cpp
@@ -0,0 +1,12 @@
+vector<int> findArrayIntersection(vector<int> &arr1,n,vector<int> &arr2,m){
+  vector<int> ans;
+for(int i=0;i<n;i++){
+int element = arr1[i];
+  for(int j=0;j<m;j++){
+  if(element==arr2[j]){
+   ans.push_back(element);
+    arr2[j]=INT_MIN;
+  }
+  }
+}
+} 
