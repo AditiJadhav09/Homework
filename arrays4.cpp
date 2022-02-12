b8174bf418857c67576f3f8b871ddc43c5c70601
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2022-02-12 18:11:32 +0530

    Update arrays4.cpp

--- arrays4.cpp
+++ arrays4.cpp
@@ -12,3 +12,35 @@ int element = arr1[i];
 }
   return ans;
 } 
+
+OPTIMISATION 1
+  class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        int i = 0, j = 0;
+        vector<int> v;
+        sort(a, a + n);
+        sort(b, b + m);
+        while(i < n && j < m)
+        {
+            if(i > 0 && a[i] == a[i-1])
+            {
+                i++; 
+                continue;
+            }
+            if(a[i] < b[j])
+                i++;
+            else if(a[i] > b[j])
+                j++;
+            else if(a[i] == b[j])
+            {
+                v.push_back(a[i]);
+                i++;  j++;
+            }
+        }
+        return v.size();
+    }
+};
