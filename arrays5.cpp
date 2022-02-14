62e9f9eed749c89403eb4727ca31ab00183f6083
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2022-02-14 11:07:49 +0530

    Update arrays5.cpp

--- arrays5.cpp
+++ arrays5.cpp
@@ -8,5 +8,15 @@
  */
 vector<vector<int>> pairSum (vector<int> &arr,int s){
 vector<vector<int>> ans;
-  for(int i=0;i
+  for(int i=0;i<arr.size();i++){
+   for(int j=i+1;j<arr.size();j++)
+      if(arr[i] + arr[j]==s){
+      vector<int> temp;
+         temp.push_back(min(arr[i],arr[j]));
+         temp.push_back(max(arr[i],arr[j]));
+         ans.push_back(temp);
+      }
+  }
+   sort(arr.begin(),arr.end());
+   return ans;
 }
