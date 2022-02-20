f03a2a3a7e357367abfcfaa90ace6fc01d9e9763
Aditi Jadhav <71687378+AditiJadhav09@users.noreply.github.com>
2022-02-20 10:35:17 +0530

    Create arrays7.cpp

---
+++ arrays7.cpp
@@ -0,0 +1,40 @@
+/*Sort 0s and 1s
+ip 011001
+op 000111
+*/
+#include<iostream>
+using namespace std;
+void sortarray( int arr[],int n){
+int left=0,right=n-1;
+  while(left<right){
+    while(arr[i]==0){
+      left++;
+    }
+   while(arr[j]==1){
+     right--;
+    }
+    swap(arr[left],arr[right]);
+    left++;
+    right--;
+  }
+}
+  
+  
+int printArray(int arr[],int n){
+for(int i=0;i<n;i++){
+cout<<arr[i]<<" ";
+}
+cout<<"\n";
+}
+
+
+int main(){
+int arr[8]={0,1,1,1,0,0,0,1};
+sortarray(arr,8);
+printArray(arr,8);
+
+return 0;
+}
+
+
+
